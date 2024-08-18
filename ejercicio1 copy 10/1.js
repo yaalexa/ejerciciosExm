33922bbc67c045138e3c71a52ca37295254bcd48
valen0406 <annevalentinaj@gmail.com>
2024-08-18 18:57:02 -0500

    cambio

--- ejercicio1 copy 10/1.js
+++ ejercicio1 copy 10/1.js
@@ -17,22 +17,27 @@
   }
 
   agregarJugador(nombreJugador) {
-    const jugador = new Jugador(nombreJugador, this); 
+    let jugador = new Jugador(nombreJugador, this); 
     this.jugadores.push(jugador); 
   }
 
   encontrarJugador(nombreJugador) {
      for (let i = 0; i < this.jugadores.length; i++) {
         let jugador = this.jugadores[i];
-        if (jugador.nombre === nombreJugador) {
+        if (jugador.nombre == nombreJugador) {
             return jugador; 
         }
     }
   }
-
+ listarJugador() {
+     for (let i = 0; i < this.jugadores.length; i++) {
+        let jugador = this.jugadores[i];
+            return jugador; 
+        
+    }
+  }
   listarPuntajes() {
      let resultados = [];
-
     for (let i = 0; i < this.jugadores.length; i++) {
         let jugador = this.jugadores[i];
         let resultado = jugador.nombre+jugador.puntaje;
@@ -43,21 +48,67 @@
   }
 }
 
-export class Liga {
+ class Liga {
   constructor() {
     this.equipos = []; // Agregación
   }
 
   agregarEquipo(equipo) {
-    this.equipos.push(equipo);
+      this.equipos.push(equipo);
+      return equipo;
   }
 
   obtenerEquipo(nombreEquipo) {
     for (let i = 0; i < this.equipos.length; i++) {
         let equipo = this.equipos[i];
-        if (equipo.nombre === nombreEquipo) {
+        if (equipo.nombre == nombreEquipo) {
             return equipo; 
         }
     }
   }
 }
+let liga = new Liga();
+
+document.querySelector('#crearEquipo').addEventListener('click', () => {
+    let nombre = document.querySelector('#nombre-equipo').value;
+    let equipo = new Equipo(nombre);
+    liga.agregarEquipo(equipo);
+    let item = document.createElement('li');
+    item.innerText = equipo.nombre;
+    document.querySelector('#lista-equipo').appendChild(item);
+});
+
+document.querySelector('#agregarJugador').addEventListener('click', ()=>{
+    let nombreEquipo = document.querySelector('#nombre-equipo-jugador').value;
+    let nombreJugador = document.querySelector('#nombre-jugador').value;
+    let equipo = liga.obtenerEquipo(nombreEquipo);
+    try {
+        equipo.agregarJugador(nombreJugador);
+    } catch (error) {
+    }
+ 
+   let item = document.createElement('li');
+    item.innerText = equipo.listarJugador().nombre;
+    document.querySelector('#lista-jugador').appendChild(item);
+});
+document.querySelector('#asignarPuntos').addEventListener('click', () => {
+    let nombreEquipo = document.querySelector('#nombre-equipo-puntos').value;
+    let nombreJugador = document.querySelector('#nombre-jugador-puntos').value;
+    let puntos = parseInt(document.querySelector('#puntos').value);
+    let equipo = liga.obtenerEquipo(nombreEquipo);
+    let jugador = equipo.encontrarJugador(nombreJugador);
+    jugador.asignarPuntos(puntos);
+    document.querySelector('#puntajes').innerText=jugador.puntaje;
+});
+document.querySelector('#listarPuntajes').addEventListener('click', () => {
+    let nombreEquipo = document.querySelector('#nombre-equipo-listar').value;
+    let equipo = liga.obtenerEquipo(nombreEquipo);
+    let listaPuntajes = equipo.listarPuntajes();
+    let lista = document.querySelector('#lista-puntajes');
+   for (let i = 0; i < listaPuntajes.length; i++) {
+    let puntaje = listaPuntajes[i]; 
+    const item = document.createElement('li');
+    item.innerText = puntaje;
+    document.querySelector('#lista-puntajes').appendChild(item);
+}
+});
