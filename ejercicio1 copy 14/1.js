51f90905723a76ab789fb9a5e85de6ebc66401a9
valen0406 <annevalentinaj@gmail.com>
2024-08-19 00:25:15 -0500

    cambio

---
+++ ejercicio1 copy 14/1.js
@@ -0,0 +1,7 @@
+/**implemente el archivo js para el ejercicio html realizado que
+ * le permita gestionar proyectos y miembros del equipo, 
+ * donde los usuarios puedan agregar proyectos y miembros ,
+ *  asignar miembros a proyectos y visualizar los detalles actuales. 
+ *  utilice las clases Proyecto y Miembro con agregación para modelar 
+ * la relación muchos a muchos entre proyectos y miembros.
+ * la aplicacion debe mostrar la información de proyectos y miembros. */
